f01d866613bcd05de6c69a5d1998e7d08bcdbb0b
Ramanathan SKS <58496877+Ramanathan87@users.noreply.github.com>
2020-04-03 23:39:54 +0530

    Update ser.js

--- ser.js
+++ ser.js
@@ -10,18 +10,35 @@ http.listen(port, function(){
     console.log('This is listening to => http://localhost:4200');
   });
 connect=[];
+let lodusr=[];
 io.on('connection',socket => {
         connect.push(socket);
         console.log("Connected socket => %s socket",connect.length);
     socket.on('disconnect',function(socket){
-        connect.splice(connect.indexOf(socket),1)
+        connect.splice(connect.indexOf(socket),1);
+        lodusr.splice(lodusr.indexOf(socket),1);
         console.log("Disconnected socket =>remaining : %s  socket ",connect.length);
+        if (connect.length == 0){
+            lodusr=[];
+        }
+    });
+    socket.on('dis',function(data){
+        io.emit('bye',{des:data +" Bye Byeee...",usr:data});
+    });
+    socket.on('user',function(user){
+        if(lodusr.includes(user) && lodusr.length>0 ){
+            socket.emit('errusr',{des:user +" is Already taken.. try another Name",usr:""});
+        }
+        else{
+            lodusr.push(user);
+            io.emit('broadcast',{des:user +" joined the Chat",usr:user});
+        }
     });
     socket.on('send message',function(msg,user){
         io.emit('new message',{msg:msg,usr:user});
     });
-    socket.on('user',function(user){
-        io.emit('broadcast',{des:user +" joined the Chat",usr:user});
-    });
-    setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+    setInterval(() =>{
+        d=new Date();
+        io.emit('time',d.getDate()+"     -     "+(d.getMonth()+1)+"     -     "+(d.getFullYear())+"                       "+d.getHours()+"     :     "+d.getMinutes()+"     :     "+d.getSeconds());
+    }, 1000);
 });
